c1430215c3ca62775f0b57cfe39abab714ea2f59
Chak <chak.koppula199@gmail.com>
2020-07-20 00:58:47 +0100

    evaluated model using accuracy, precision, recall and f1 score

--- Fake_News_Detector.py
+++ Fake_News_Detector.py
@@ -62,10 +62,11 @@ classifier.fit(tfidf_train, y_train)
 
 # Input has to be a 2D array
 y_pred_single = classifier.predict([tfidf_test[0]])
+print("Predicting Single Result\n")
 # Below line prints the entire article
 # print(f"Input is {X_test[0]}")
 print(f"Actual label is {y_test[0]}")
-print(f"Predicted label is {y_pred_single}")
+print(f"Predicted label is {y_pred_single}\n\n")
 
 
 """
@@ -74,4 +75,21 @@ print(f"Predicted label is {y_pred_single}")
 
 y_pred = classifier.predict(tfidf_test)
 print("Prediction, Actual")
-print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), axis=1))
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), axis=1), end='\n\n')
+
+"""
+--------------Making the Confusion Matrix and Evaluating the Model---------------
+"""
+
+# Gives confusion matrix C such that Cij is equal to the number of observations known to be in group i and predicted to be in group j.
+cm = confusion_matrix(y_test, y_pred)
+true_positive = cm[1][1]
+true_negative = cm[0][0]
+false_positive = cm[0][1]
+false_negative = cm[1][0]
+
+print("\nModel Evaluation")
+print(f"Accuracy is {accuracy_score(y_test, y_pred)}")
+print(f"Precision is {true_positive / (true_positive + false_positive)}")
+print(f"Recall is {true_positive / (true_positive + false_negative)}")
+print(f"F1 score is {f1_score(y_test, y_pred, labels=['REAL', 'FAKE'], pos_label='REAL')}")
